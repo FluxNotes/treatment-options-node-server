3667d15311072dd5fcef48b325fd35b65c5aa87f
Karen <kzheung@mitre.org>
2018-08-15 10:06:28 -0400

    Initial server implementation, just returned hardcoded data

---
+++ dataaccess/TreatmentDataAccess.jsx
@@ -0,0 +1,25 @@
+import HardCodedTreatmentData from './HardCodedTreatmentData.json';
+
+class TreatmentDataAccess {
+    getTreatmentData(disease, race, grade) {
+
+        const treatmentOptions = ['Chemo', 'Chemo+Rad', 'Hormonal', 'Radiation',
+                                'Surgery', 'Surg+Rad', 'No-Treatment'];
+
+        let deceasedSeries = [];
+        let aliveSeries = [];
+       
+        HardCodedTreatmentData.forEach((v) => {
+            if(v.Disease === disease && v.Race === race && v['Dx-Grade'] === grade && v['Is-Alive'] === 'Dead'){
+                deceasedSeries.push([ treatmentOptions.indexOf(v['Treat-option'])  , v['Survival-months'] ]);
+            }
+            if(v.Disease === disease && v.Race === race && v['Dx-Grade'] === grade && v['Is-Alive'] === 'Alive'){
+                aliveSeries.push([ treatmentOptions.indexOf(v['Treat-option'])  , v['Survival-months'] ]);
+            }  
+        });
+        
+        return [aliveSeries, deceasedSeries];
+    }
+}
+
+export default TreatmentDataAccess;
