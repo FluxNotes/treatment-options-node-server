73899accfb21e6b53aad3591bcb25a86216f8e0d
Karen <kzheung@mitre.org>
2018-09-01 18:52:27 -0400

    Unnested the promise

--- dataaccess/MongoDataSource.js
+++ dataaccess/MongoDataSource.js
@@ -14,20 +14,22 @@ export default class MongoDataSource {
         .then( database => {
             const collection = database.collection('Treatment Options Data');
             const result = collection.find({Disease: disease}).toArray();
+            return result;
+        })
+        .then( result2 => {
             let alive = [];
             let deceased = [];
-            return result.then(function(result2) {
-                result2.forEach(entry => {
-                    if((race === "undefined" ? true : (entry.Race === race)) && (dxGrade === "undefined" ? true: (entry['Dx-Grade'] === dxGrade)) && entry['Is-Alive'] === 'Alive'){
-                        alive.push([ treatmentOptions.indexOf(entry['Treat-option'])  , entry['Survival-months'] ]);
-                    }
-                    if((race === "undefined" ? true: (entry.Race === race)) && (dxGrade === "undefined" ? true: (entry['Dx-Grade'] === dxGrade)) && entry['Is-Alive'] === 'Dead'){
-                        deceased.push([ treatmentOptions.indexOf(entry['Treat-option'])  , entry['Survival-months'] ]);
-                    }
-                });
-                return([alive, deceased]);
-            }); 
-        }) .catch(function(err) {
+            result2.forEach(entry => {
+                if((race === "undefined" ? true : (entry.Race === race)) && (dxGrade === "undefined" ? true: (entry['Dx-Grade'] === dxGrade)) && entry['Is-Alive'] === 'Alive'){
+                    alive.push([ treatmentOptions.indexOf(entry['Treat-option'])  , entry['Survival-months'] ]);
+                }
+                if((race === "undefined" ? true: (entry.Race === race)) && (dxGrade === "undefined" ? true: (entry['Dx-Grade'] === dxGrade)) && entry['Is-Alive'] === 'Dead'){
+                    deceased.push([ treatmentOptions.indexOf(entry['Treat-option'])  , entry['Survival-months'] ]);
+                }
+            });
+            return([alive, deceased]);
+        })
+        .catch(function(err) {
             return err.message;
         }) ; 
     }
