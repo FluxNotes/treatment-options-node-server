0e747087a107e6ffea6c036b50249b6d2eae60a6
Karen <kzheung@mitre.org>
2018-10-09 09:21:13 -0400

    Handles input of multiple criteria and removes undefined ones

--- dataaccess/MongoDataSource.js
+++ dataaccess/MongoDataSource.js
@@ -6,21 +6,18 @@ const databaseName = 'treatment-options';
 
 export default class MongoDataSource {
 
-   findTreatmentOptionsByPatientStats = (params) => {
-        const cleanParams = (params) => {
-            Object.keys(params).forEach(key => {
-                if (params[key] === 'undefined'){
-                    delete params[key];
-                }
-            });
-        }
-
+   findTreatmentOptionsByPatientStats = async (params) => {
+   
+        Object.keys(params).forEach(key => params[key].value === undefined ? delete params[key] : '');
+    
         let database;
         const demoFlag = process.argv.length > 2 && process.argv[2] === '--nodemo' ? false : true;
         return MongoClient.connect("mongodb://" + mongoHost + ":" + mongoPort + "/" + databaseName)
-        .then( database => {
+        .then( async (database) => {
+            //console.log('disease is', params['disease'].value)
             const collection = database.collection('Treatment Options Data');
-            const result = collection.find({Disease: disease}).toArray();
+            const result = await collection.find({Disease: params['disease'].value}).toArray();
+        
             return result;
         })
         .then( mongoData => {
@@ -29,19 +26,31 @@ export default class MongoDataSource {
             let deceased = [];
             result2.forEach(entry => {
                 //if((race === "undefined" ? true : (entry.Race === race)) && (dxGrade === "undefined" ? true: (entry['Dx-Grade'] === dxGrade)) && entry['Is-Alive'] === 'Alive'){
-                if (Object.keys(cleanParams).forEach(key => {
-                    entry[key] === cleanParams[key]
+               /*  if (Object.keys(params).forEach(key => {
+                    entry[key] === params[key].value
                 }) && entry['Is-Alive'] === 'Alive'){
+                    console.log(params[key])
+                    console.log('adding to alive array')
                     alive.push([ entry['Treat-option'], entry['Survival-months'] ]);
-                }
+                } */
+                Object.keys(params).forEach(key => {
+                    if(entry[key] === params[key].value && entry['Is-Alive'] === 'Alive') {
+                        alive.push([ entry['Treat-option'], entry['Survival-months'] ]);
+                    }
+                    if(entry[key] === params[key].value && entry['Is-Alive'] === 'Dead') {
+                        deceased.push([ entry['Treat-option'], entry['Survival-months'] ]);
+                    }
+                })
                 //if((race === "undefined" ? true: (entry.Race === race)) && (dxGrade === "undefined" ? true: (entry['Dx-Grade'] === dxGrade)) && entry['Is-Alive'] === 'Dead'){
-                if (Object.keys(cleanParams).forEach(key => {
-                    entry[key] === cleanParams[key]
+               /*  if (Object.keys(params).forEach(key => {
+                    entry[key] === params[key].value
                 }) && entry['Is-Alive'] === 'Dead') { 
+                    console.log('adding to deceased array')
                     deceased.push([ entry['Treat-option'], entry['Survival-months'] ]);
-                }
+                } */
             });
-            return({isDemo: demoFlag, data:{alive: alive, deceased: deceased}});
+         
+            return([alive, deceased]);
         })
     }
 
